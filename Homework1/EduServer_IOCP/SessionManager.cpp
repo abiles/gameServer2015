20a38378e673a2fc62a41589ae5bd83848ff8c19
abiles <pgalpga@hanmail.net>
2015-04-06 00:45:06 +0900

    Homework 1

---
+++ Homework1/EduServer_IOCP/SessionManager.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "ClientSession.h"
+#include "SessionManager.h"
+
+SessionManager* GSessionManager = nullptr;
+
+ClientSession* SessionManager::CreateClientSession(SOCKET sock)
+{
+	ClientSession* client = new ClientSession(sock);
+
+	//TODO: lock으로 보호할 것
+	FastSpinlockGuard lockGuard(mLock);
+
+	{
+		mClientList.insert(ClientList::value_type(sock, client));
+	}
+
+
+
+	return client;
+}
+
+
+void SessionManager::DeleteClientSession(ClientSession* client)
+{
+	//TODO: lock으로 보호할 것
+	
+	FastSpinlockGuard lockGuard(mLock);
+
+	{
+		mClientList.erase(client->mSocket);
+	}
+
+	
+	delete client;
+}
---
+++ Homework1/EduServer_IOCP/SessionManager.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <map>
+#include <WinSock2.h>
+#include "FastSpinlock.h"
+
+class ClientSession;
+
+class SessionManager
+{
+public:
+	SessionManager() : mCurrentConnectionCount(0)	{}
+
+	ClientSession* CreateClientSession(SOCKET sock);
+
+	void DeleteClientSession(ClientSession* client);
+
+	int IncreaseConnectionCount() { return InterlockedIncrement(&mCurrentConnectionCount); }
+	int DecreaseConnectionCount() { return InterlockedDecrement(&mCurrentConnectionCount); }
+
+
+private:
+	typedef std::map<SOCKET, ClientSession*> ClientList;
+	ClientList	mClientList;
+
+	//TODO: mLock; ¼±¾ð
+	FastSpinlock mLock;
+
+	volatile long mCurrentConnectionCount;
+};
+
+extern SessionManager* GSessionManager;
