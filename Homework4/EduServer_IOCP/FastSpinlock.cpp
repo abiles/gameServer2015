796e33d24c824eb1f14023a588e69a719f56693c
abiles <pgalpga@hanmail.net>
2015-04-26 00:55:18 +0900

    enter leave lock

--- Homework4/EduServer_IOCP/FastSpinlock.cpp
+++ Homework4/EduServer_IOCP/FastSpinlock.cpp
@@ -26,15 +26,29 @@ void FastSpinlock::EnterWriteLock()
 		while (mLockFlag & LF_WRITE_MASK)
 			YieldProcessor();
 
+		// mLockFlag가 READ_MASK라면 통과함
+		// mLockFlag가 0이라면 통과함
+		// mLockFlag가 WRITE_FLAG라면 저지당함
+		// & LF_WRITE_MASK를 해주는 이유는 READ 상태에서도 통과하게 하기 위해??
+		// 이 if문을 통과 했다면 read하고 있는 애들이 있으면서 쓰려고 하는 1명이 있는 경우
+		// 혹은 read할 애는 없고 쓰려고 하는 애가 1명 있는 경우 
+
 		if ((InterlockedAdd(&mLockFlag, LF_WRITE_FLAG) & LF_WRITE_MASK) == LF_WRITE_FLAG)
 		{
 			/// 다른놈이 readlock 풀어줄때까지 기다린다.
+			// 뭔가 새로운 Write를 하기 위해서는 읽고 있는 애들도 일을 다 마쳐야함
+			// 이 while문이 종료 되기 위해서는 read하는 내용이 없어져야 하고
+			// 그러면 read하는 애가 있는 상태로 return 된다
+			// 즉 mLockFlog == LF_WRITE_FLAG인 상태다
 			while (mLockFlag & LF_READ_MASK)
 				YieldProcessor();
 
 			return;
 		}
 
+		// 바로 위에 있는 if문을 만족하지 못하고 여기로 오면
+		// if문을 진행하는 동안 LF_WRITE_FLAG를 한번 더해줬기 때문에
+		// 그만큼을 빼준다
 		InterlockedAdd(&mLockFlag, -LF_WRITE_FLAG);
 	}
 
@@ -66,7 +80,15 @@ void FastSpinlock::EnterReadLock()
 		// else
 			// mLockFlag 원복
 
+		// WRITE_MAKS거나 WRITE_FLAG라면
+		// if문을 통과하지 못한다.
+		if (InterlockedAdd(&mLockFlag, 1) & LF_WRITE_MASK == 0)
+		{
+			return;
+		}
 
+
+		InterlockedAdd(&mLockFlag, -1);
 		
 	}
 }
@@ -74,6 +96,7 @@ void FastSpinlock::EnterReadLock()
 void FastSpinlock::LeaveReadLock()
 {
 	//TODO: mLockFlag 처리 
+	InterlockedAdd(&mLockFlag, -1);
 	
 
 	if (mLockOrder != LO_DONT_CARE)
